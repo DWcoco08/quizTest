4f78876cb7d03ce4398315cb2cd079743d0377e5
DWuy <dnduchuy08@gmail.com>
2025-06-29 21:54:10 +0700

    code sidebar for admin and install some library

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,5 +1,20 @@
-const Admin = (props) => {
-  return <div>Admin Component</div>;
-};
+import SideBar from "./SideBar";
+import "./Admin.scss";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
+const Admin = (props) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <div className="admin-container">
+      <div className="admin-sidebar">
+        <SideBar collapsed={collapsed} />
+      </div>
+      <div className="admin-content">
+        <FaBars onClick={() => setCollapsed(!collapsed)} />
+      </div>
+    </div>
+  );
+};
 export default Admin;
--- src/components/Header/NavBar_Header.jsx
+++ src/components/Header/NavBar_Header.jsx
@@ -3,7 +3,6 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink } from "react-router-dom";
-import HomePage from "../Home/HomePage";
 
 const NavBar_Header = () => {
   return (
