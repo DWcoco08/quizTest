0f75465dbb3575c15ebf873963c2a27b94494c53
DWuy <dnduchuy08@gmail.com>
2025-07-01 21:51:17 +0700

    validate data with react-toatify

--- src/components/Admin/Content/ModalCreateUser.jsx
+++ src/components/Admin/Content/ModalCreateUser.jsx
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FcPlus } from "react-icons/fc";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const ModalCreateUser = (props) => {
   const { show, setShow } = props;
@@ -30,9 +31,28 @@ const ModalCreateUser = (props) => {
     }
   };
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
   const handleSubmitCreateUser = async () => {
     // validate
-    //call APIS
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      toast.error("Invalid email!");
+      return;
+    }
+
+    if (!password) {
+      toast.error("Password is required!");
+      return;
+    }
+
+    // call APIS
     // let data = {
     //   email: email,
     //   password: password,
@@ -42,6 +62,7 @@ const ModalCreateUser = (props) => {
     // };
     // console.log(">>> check data: ", data);
 
+    // submit data
     const data = new FormData();
     data.append("email", email);
     data.append("password", password);
@@ -53,7 +74,16 @@ const ModalCreateUser = (props) => {
       "http://localhost:8081/api/v1/participant",
       data
     );
-    console.log(">>> check res: ", res);
+
+    // check res
+    console.log(">>> check res: ", res.data);
+    if (res.data && res.data.EC === 0) {
+      toast.success(res.data.EM);
+      handleClose();
+    }
+    if (res.data && res.data.EC !== 0) {
+      toast.error(res.data.EM);
+    }
   };
 
   return (
@@ -85,6 +115,7 @@ const ModalCreateUser = (props) => {
               <input
                 type="password"
                 className="form-control"
+                autoComplete="current-password"
                 value={password}
                 onChange={(event) => setPassword(event.target.value)}
               />
