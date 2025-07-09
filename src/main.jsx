46d4974d7f61506614816a484926a66ad9d1cc68
DWuy <dnduchuy08@gmail.com>
2025-07-09 19:18:22 +0700

    using redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
 import ReactDOM from "react-dom/client";
 import "nprogress/nprogress.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
   // </StrictMode>
 );
